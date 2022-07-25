a206512b6ee2451f667f6ba54f8bdb959487319e
Jason Kwak <ralph9696@naver.com>
2022-07-25 19:58:45 -0400

    commit tree d8ab68a1999b55819fbc2467c3a8b1e68d291fee

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+  }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+const names = ['Charlie', 'Samip', 'Ali'];
+const event = 'birthday';
+let message = [];
+
+function writeCards(names, event) {
+    for (let a = 0; a < names.length; a++) {
+        message.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+
+let n
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
